c51ae885d2c80bc3a04f03e12369753b349a7421
Anan Suliman <anan@laconi.us>
2021-05-01 00:44:02 +0300

    Initial Commit

---
+++ MacNews/MacNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacNewsApp.swift
+//  MacNews
+//
+//  Created by Anan Suliman on 01/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MacNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
